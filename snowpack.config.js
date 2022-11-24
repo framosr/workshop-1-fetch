79190d3b9b7cc977eee173f2fa52e4b1336559dc
Fidel Ramos <fidelramos62@gmail.com>
2022-11-23 20:04:31 -0700

    first commit- agregue archivos-

---
+++ snowpack.config.js
@@ -0,0 +1,10 @@
+/** @type {import("snowpack").SnowpackUserConfig } */
+module.exports = {
+  mount: {
+    public: "/",
+    src: "/_dist_",
+  },
+  buildOptions: {
+    baseUrl: '"https://framos.github.io/workshop-1-fetch"',
+  },
+};
